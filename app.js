986e3c3cd0b90f8a77e832599cf5db17ee9cbcac
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2017-01-09 17:05:03 +0100

    update

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ app.controller("TodoController", function($scope) {
     $scope.title = "Bonjour";
     $scope.list = ["Manger", "boire", "respirer", "courir", "rire", "jouer"];
     $scope.addItem = function(item) {
+      if (item === null || item === undefined || item === "") {
+        return;
+      }
         $scope.list.push(item);
         $scope.newItem = null;
     };
