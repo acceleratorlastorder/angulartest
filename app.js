ef0015bfc8412f68a0a396b23c26ae60b8b3c02d
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2017-01-10 12:00:11 +0100

    update

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const app = angular.module("TodoApp", []);
 app.controller("TodoController", function($scope) {
     $scope.title = "Todo list";
-    $scope.list = ["Manger", "boire", "respirer", "courir", "rire", "jouer"];
-    $scope.removeItem = function(index){
-      $scope.list.splice(index, 1);
+    $scope.list = ["Manger", "Boire", "Respirer", "Courir", "Rire", "Jouer"];
+    $scope.removeItem = function(index) {
+        $scope.list.splice(index, 1);
     }
+
+    $scope.deleteAll = function() {
+        $scope.list.splice(0, $scope.list.length);
+    };
+
     $scope.addItem = function(item) {
         if (item === null || item === undefined || item === "") {
             return;
