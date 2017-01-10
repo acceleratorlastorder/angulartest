55d58d5f11123b570052bdd18f7fe27bd2067282
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2017-01-10 10:31:23 +0100

    delete item on click is working

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const app = angular.module("TodoApp", []);
 app.controller("TodoController", function($scope) {
     $scope.title = "Bonjour";
     $scope.list = ["Manger", "boire", "respirer", "courir", "rire", "jouer"];
+    $scope.removeItem = function(index){
+      $scope.list.splice(index, 1);
+    }
     $scope.addItem = function(item) {
-      if (item === null || item === undefined || item === "") {
-        return;
-      }
+        if (item === null || item === undefined || item === "") {
+            return;
+        }
         $scope.list.push(item);
         $scope.newItem = null;
     };
